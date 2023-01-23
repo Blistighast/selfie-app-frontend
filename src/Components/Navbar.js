13cc8f76ddb81999e158e5232f38c326a2b4198d
Brian Guterl <brianguterl99@gmail.com>
2023-01-23 02:36:59 -0500

    changed some css

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => (
   <nav>
     <h1>Yo, Where You At?</h1>
-    <NavLink to="/">Home</NavLink>
+    <NavLink to="/">Enter</NavLink>
     <NavLink to="locations-list">Locations</NavLink>
   </nav>
 );
