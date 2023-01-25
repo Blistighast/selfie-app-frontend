ee7a38673d81cde7e3f305c02bfdb0d9ed8b3c6f
Brian Guterl <brianguterl99@gmail.com>
2023-01-24 19:55:11 -0500

    deleted some console logs

--- src/utils/weatherFetch.js
+++ src/utils/weatherFetch.js
@@ -2,7 +2,6 @@ import server_url from "./server_url";
 
 const weatherFetch = async (lat, lon) => {
   const api_url = `${server_url}/weather/${lat},${lon}`;
-  console.log(api_url);
   const res = await fetch(api_url);
   const data = await res.json();
   return data;
